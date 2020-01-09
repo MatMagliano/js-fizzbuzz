446839debd0d4681032bc3336fb80060785bde18
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-09 16:08:23 +0100

    aggiunta con while

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 // alert('ciao!!')
-var numeri = [1];
 
-for (var i = 1; i <= 100; i++) {
+
+//metodo con for
+
+// for (var i = 1; i <= 100; i++) {
+//   if ((i %5 == 0) && (i %3 == 0)) {
+//     console.log('FizzBuzz');
+//     document.getElementById('lista_numeri').innerHTML  += '<li>' + 'FizzBuzz' + '</li>';
+//   } else if (i % 5 == 0) {
+//       console.log('Buzz');
+//       document.getElementById('lista_numeri').innerHTML  += '<li>' + 'Buzz' + '</li>';
+//   } else if (i % 3 == 0) {
+//       console.log('Fizz');
+//       document.getElementById('lista_numeri').innerHTML  += '<li>' + 'Fizz' + '</li>';
+//   } else {
+//     console.log(i);
+//     document.getElementById('lista_numeri').innerHTML  += '<li>' + i + '</li>';
+//   }
+// }
+// document.getElementById('lista_numeri').classList.add('list_style');
+
+
+//metodo con while
+
+var i = 1;
+while (i <= 100) {
   if ((i %5 == 0) && (i %3 == 0)) {
     console.log('FizzBuzz');
     document.getElementById('lista_numeri').innerHTML  += '<li>' + 'FizzBuzz' + '</li>';
@@ -12,8 +35,9 @@ for (var i = 1; i <= 100; i++) {
       console.log('Fizz');
       document.getElementById('lista_numeri').innerHTML  += '<li>' + 'Fizz' + '</li>';
   } else {
-    console.log(numeri[i]);
-    document.getElementById('lista_numeri').innerHTML  += '<li>' + numeri[i] + '</li>';
+    console.log(i);
+    document.getElementById('lista_numeri').innerHTML  += '<li>' + i + '</li>';
   }
+  i++;
 }
 document.getElementById('lista_numeri').classList.add('list_style');
